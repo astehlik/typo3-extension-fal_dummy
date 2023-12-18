436ed6095b8b7117e4aa73911ead326c4dacfb04
Alexander Stehlik <alex@stehlik-online.de>
2023-12-18 01:16:59 +0100

    [TASK] Streamline required TYPO3 versions
    Require TYPO3 versions without security issues in composer.json.
    
    Adjust required versions in ext_emconf.php to match those in
    composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,10 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'version' => '0.1.0',
     'constraints' => [
-        'depends' => [
-            'php' => '5.3.0-0.0.0',
-            'typo3' => '6.2.2-7.6.99',
-        ],
+        'depends' => ['typo3' => '8.7.55-9.5.99'],
         'conflicts' => [],
         'suggests' => [],
     ],
